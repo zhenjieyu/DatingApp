62dac62bc7386fa53f8e04140080c938756607c8
Peter Yu <peter.yu@trapezegroup.com>
2020-10-30 15:44:46 -0400

    first commit' this is my first cmmit

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace API.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        { 
+
+        }
+
+        public DbSet<AppUser> Users{ get; set; }
+    }
+}
