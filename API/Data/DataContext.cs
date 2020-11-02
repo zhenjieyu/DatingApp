5e82ff44668e8b3bde20e73885e96f1a8321730d
Peter Yu <peter.yu@trapezegroup.com>
2020-11-01 19:08:38 -0500

    Endof Section 3

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
-
 namespace API.Data
 {
-    public class DataContext:DbContext
+    public class DataContext : DbContext
     {
         public DataContext(DbContextOptions options) : base(options)
-        { 
-
+        {
         }
 
-        public DbSet<AppUser> Users{ get; set; }
+        public DbSet<AppUser> Users { get; set; }
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -12,28 +12,26 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
 namespace API
 {
     public class Startup
     {
-         private readonly IConfiguration _config;
+        private readonly IConfiguration _config;
 
         public Startup(IConfiguration config)
         {
-             _config = config; 
+            _config = config;
         }
-         
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options =>
-                {
-                    options.UseSqlite(_config.GetConnectionString("DefaultConnection") );
-                }
-                );
+            {
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
+            });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +45,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
